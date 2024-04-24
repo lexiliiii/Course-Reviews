da22f428884ffb58275d91d309a37ad46b344aab
sophieyann <drc5dr@virginia.edu>
2024-04-24 14:32:15 -0400

    Created Classes

---
+++ src/main/java/edu/virginia/sde/reviews/CourseReviewApplication.java
@@ -0,0 +1,15 @@
+package edu.virginia.sde.reviews;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CourseReviewApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
