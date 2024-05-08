a958cdac63e3ba596d1674033e4e01fc91a5025f
sophieyann <drc5dr@virginia.edu>
2024-05-07 21:37:42 -0400

    Finished Course Adding Action

--- src/main/java/edu/virginia/sde/reviews/DatabaseReviews.java
+++ src/main/java/edu/virginia/sde/reviews/DatabaseReviews.java
@@ -143,6 +143,7 @@ public class DatabaseReviews {
             pstmt.setString(3, title);
             pstmt.setDouble(4, avgPoint);
             pstmt.executeUpdate();
+            commit();
 
         } catch (SQLException e) {
             rollback();
@@ -189,6 +190,7 @@ public class DatabaseReviews {
             pstmt.setInt(3, courseNumber);
             pstmt.setInt(4, rating);
             pstmt.executeUpdate();
+            commit();
 //            if (affectedRows > 0) {
 //                System.out.println("Review added successfully.");
 //            } else {
@@ -244,6 +246,7 @@ public class DatabaseReviews {
             pstmt.setString(5, comment);
 
             pstmt.executeUpdate();
+            commit();
 //            if (affectedRows > 0) {
 //                System.out.println("Review added successfully for course: " + courseMnemonic);
 //            } else {
