d0b7dc2abf65d670cf442a468c0bd13e2c29d408
xiaorui <nsq8va@virginia.edu>
2024-05-07 21:40:05 -0400

    My review commented version

--- src/main/java/edu/virginia/sde/reviews/MyReviewController.java
+++ src/main/java/edu/virginia/sde/reviews/MyReviewController.java
@@ -13,81 +13,54 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.control.Button;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyReviewController {
-    public MyReviewController(Stage stage){
+    public MyReviewController(Stage stage, String username){
         stage.setTitle("My Review");
         Label myreview = new Label( "My Review" );
 
 
-        FlowPane root=new FlowPane();
-        root.getChildren().add(myreview);
-//        stage.setTitle( "Course Search" );
-//
-//        Label searchLabel = new Label( "Course Search" );
-//
-//        Label mnemonicLabel = new Label( "Subject Mnemonic: " );
-//        TextField mnemonicInput = new TextField();
-//        Label numberLabel = new Label( "Course Number: " );
-//        TextField numberInput = new TextField();
-//        Label titleLabel = new Label( "Course Title: " );
-//        TextField titleInput = new TextField();
-//
-//        // TODO: Action for Searching
-//        Button searchButton = new Button( "Search" );
-//        searchButton.setOnAction(event -> {
-////            stage.close();
-//        });
-//
-//        HBox inputBox = new HBox( 10 );
-//        inputBox.getChildren().addAll( mnemonicLabel, mnemonicInput, numberLabel, numberInput, titleLabel, titleInput, searchButton );
-//        inputBox.setAlignment( Pos.CENTER );
-//
-//        VBox searchBox = new VBox( 10 );
-//        searchBox.getChildren().addAll( searchLabel, inputBox );
-//        searchBox.setAlignment( Pos.CENTER );
+
+
 //
 //
-//        // Testing Course SetUP:
-//        Course course1 = new Course( "CS", 3140, "Software Development Essentials", 0.0);
-//        course1.setAvgRating( 2.356 );
-//        Course course2 = new Course( "CS", 2130, "Computer Systems and Organization", 0.0);
-//        course2.setAvgRating( 2.07 );
+        // Testing Review:
+        MyReview review1 = new MyReview("iris","CS",3140,1);
+        MyReview review2 = new MyReview("lexie","CS",3140,1);
 //
 //
-//        // TODO: Sort out courses base on the search
+////        // TODO: Sort out courses base on the search
 //        ListView<Course> list = new ListView<Course>();
-//        ObservableList<Course> items = FXCollections.observableArrayList( course1, course2 );
+//        ObservableList<Course> items = null;
+//        try {
+//            items = viewableMyReviews(username);
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
 //        list.setItems( items );
 //        list.setPrefWidth( 800 );
+
 //
 //        // TODO: Set Action for clicking each row.
 //        list.setOnMouseClicked(event -> {
-////            LogInController login = new LogInController( stage );
+//            CourseReviewController courseReview=new CourseReviewController(stage, username);
 //        });
-//
-//        Button addButton = new Button( "Add Courses" );
-//        addButton.setOnAction(event -> {
-//            CourseAdding add = new CourseAdding( stage );
-//        });
-//
-//        // TODO: Connect to My Reviews Scene.
-//        Button myReviewButton = new Button( "My Review" );
-//        myReviewButton.setOnAction(event -> {
-//            MyReviewController review=new MyReviewController(stage);
-////            stage.close();
-//        });
-//
-//        Button logOutButton = new Button( "Log Out" );
-//        logOutButton.setOnAction(event -> {
-//            LogInController login = new LogInController( stage );
-//        });
-//
-//        HBox functionBox = new HBox( 10 );
-//        functionBox.getChildren().addAll( addButton, myReviewButton, logOutButton );
-//        functionBox.setAlignment( Pos.CENTER );
-//
-//
-//        GridPane root = new GridPane();
+
+        Button logOutButton = new Button( "Log Out" );
+        logOutButton.setOnAction(event -> {
+            LogInController login = new LogInController( stage );
+        });
+
+        HBox functionBox = new HBox( 10 );
+        functionBox.getChildren().addAll(  logOutButton );
+        functionBox.setAlignment( Pos.CENTER );
+
+
+        GridPane root = new GridPane();
 //        root.setHgap(10);
 //        root.setVgap(10);
 //        root.setPadding(new Insets(0, 10, 0, 10));
@@ -95,12 +68,67 @@ public class MyReviewController {
 //        root.add( searchBox, 0, 0 );
 //        root.add( list, 0, 1 );
 //        root.add( functionBox, 0, 2 );
-//
+
 
 
         Scene scene = new Scene( root,1280, 780 );
         stage.setScene(scene);
         stage.show();
     }
+//    private ObservableList<Course> viewableMyReviews(String username) throws SQLException {
+//        DatabaseReviews driver = new DatabaseReviews("reviews.sqlite" );
+//        driver.connect();
+//        driver.createTables();
+//
+//        List<MyReview> allMyReviews = driver.getMyReviews(username);
+//        ObservableList<MyReview> items = FXCollections.observableArrayList( allMyReviews );
+//        return items;
+//    }
+
+//    private ObservableList<Course> viewableReview( String mnem, String number, Integer rating) throws SQLException {
+//        if( mnem.equals( "" ) && number.equals( "" ) && rating==null ){
+//            return viewableMyReviews(username);
+//        }
+//
+//        DatabaseReviews driver = new DatabaseReviews("reviews.sqlite" );
+//        driver.connect();
+//        driver.createTables();
+//
+//        List<Course> allCourses = driver.getAllCourses();
+//        List<Course> selectedCourses = new ArrayList<Course>();
+//        mnem = mnem.toUpperCase();
+//       // title = title.toLowerCase();
+//
+//
+//        for( int i = 0; i < allCourses.size(); i++ ){
+//            boolean fit = true;
+//
+//            Course course = allCourses.get( i );
+//
+//            if( !mnem.equals( "" ) ){
+//                if( !course.getMnemonic().equals( mnem ) ){
+//                    fit = false;
+//                }
+//            }
+//            if( !number.equals( "" ) ){
+//                if( course.getCourseNumber() != Integer.parseInt( number ) ){
+//                    fit = false;
+//                }
+//            }
+//            if( !(rating==null) ){
+             //   if( !course.getCourseTitle().toLowerCase().contains( title ) ){
+//                    fit = false;
+//                }
+//            }
+
+//            if( fit == true ){
+//                selectedCourses.add( course );
+//            }
+//        }
+//
+//        ObservableList<Course> items = FXCollections.observableArrayList( selectedCourses );
+//        driver.disconnect();
+//        return items;
+//    }
 
 }
